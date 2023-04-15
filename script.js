9a27e01afff34fe196fbf94d7a6f7705fb9a418a
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-15 06:30:29 +0000

    line 9

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const combinations = (elemnts) =>{
+const combinations = (elements) =>{
   if (elements.length === 0) return [ []];
-  const firstEl =elements[0];
+  const firstEl = elements[0];
+  const rest = elements.slice(1); //like c++ substr to end of array
   
-  
+  const combsWithoutFirst = combinations(rest);
+  const combsWithFirst = [];
+
+  combsWithoutFirst.forEach(comb =>) {
+    [...comb, firstEl];
+  });
+  })
 }
 
 combinations(['a', 'b', 'c']);
